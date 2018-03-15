5ab0971b01a719c78b49f153b9841b069cd87dda
Ed <eduardo@gottabegarabi.com>
2018-03-15 11:49:40 +0000

    Revert "Quick hack to bypass serialisation errors."
    This reverts commit d479db085cbf5bc4cc2261e8270344d7333a6fb6.

--- src/SoapCore/SoapEndpointMiddleware.cs
+++ src/SoapCore/SoapEndpointMiddleware.cs
@@ -200,13 +200,8 @@ namespace SoapCore
 									break;
 								case SoapSerializer.DataContractSerializer:
 									{
-										// Hacky workaround: using XmlSerializer only until we find out why the hell DataContractSerializer is not working
-										var serializer = new XmlSerializer(elementType, null, new Type[0], new XmlRootAttribute(parameterName), parameterNs);
-										arguments.Add(serializer.Deserialize(xmlReader));
-
-										//var serializer = new DataContractSerializer(elementType, parameterName, parameterNs);
-										//arguments.Add(serializer.ReadObject(xmlReader, verifyObjectName: true));
-
+										var serializer = new DataContractSerializer(elementType, parameterName, parameterNs);
+										arguments.Add(serializer.ReadObject(xmlReader, verifyObjectName: true));
 									}
 									break;
 								default: throw new NotImplementedException();
