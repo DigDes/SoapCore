3fb37a769a422a83a043fe257024ee14748d7b85
itn3000 <itn3000@gmail.com>
2018-03-29 20:28:44 +0900

    add getting soapaction from content-type header and tests about it
    some SOAP implementation has action name in Content-Type as 'action="[action url]"'

--- src/SoapCore.Tests/Startup.cs
+++ src/SoapCore.Tests/Startup.cs
@@ -18,10 +18,16 @@ namespace SoapCore.Tests
 
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
 		{
-			app.UseSoapEndpoint<TestService>("/Service.svc", new BasicHttpBinding(), SoapSerializer.DataContractSerializer);
-			var transportBinding = new HttpTransportBindingElement();
-			var textEncodingBinding = new TextMessageEncodingBindingElement(MessageVersion.Soap12WSAddressing10, System.Text.Encoding.UTF8);
-			app.UseSoapEndpoint<TestService>("/ServiceSoap12.svc", new CustomBinding(transportBinding, textEncodingBinding), SoapSerializer.DataContractSerializer);
+			app.UseWhen(ctx => ctx.Request.Headers.ContainsKey("SOAPAction"), app2 =>
+			{
+				app2.UseSoapEndpoint<TestService>("/Service.svc", new BasicHttpBinding(), SoapSerializer.DataContractSerializer);
+			});
+			app.UseWhen(ctx => !ctx.Request.Headers.ContainsKey("SOAPAction"), app2 =>
+			{
+				var transportBinding = new HttpTransportBindingElement();
+				var textEncodingBinding = new TextMessageEncodingBindingElement(MessageVersion.Soap12WSAddressing10, System.Text.Encoding.UTF8);
+				app.UseSoapEndpoint<TestService>("/Service.svc", new CustomBinding(transportBinding, textEncodingBinding), SoapSerializer.DataContractSerializer);
+			});
 			app.UseMvc();
 		}
 	}
