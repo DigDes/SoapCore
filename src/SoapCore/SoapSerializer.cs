9579726fb88e83f0cade27b9d3076f0544838272
DerSkythe <skif@skif.ws>
2019-06-17 23:15:03 +0400

    reformat

--- src/SoapCore/SoapSerializer.cs
+++ src/SoapCore/SoapSerializer.cs
@@ -1,16 +1,16 @@
-﻿namespace SoapCore
+namespace SoapCore
 {
-    public enum SoapSerializer
-    {
-        /// <summary>
-        /// Client created from wsdl via Connected Services - Add Service Reference (see https://stackoverflow.com/a/2468182)
-        /// </summary>
-        XmlSerializer,
+	public enum SoapSerializer
+	{
+		/// <summary>
+		/// Client created from wsdl via Connected Services - Add Service Reference (see https://stackoverflow.com/a/2468182)
+		/// </summary>
+		XmlSerializer,
 
-        /// <summary>
-        /// Client created from interface via <see cref="System.ServiceModel.ChannelFactory" />
-        /// </summary>
-        DataContractSerializer,
+		/// <summary>
+		/// Client created from interface via <see cref="System.ServiceModel.ChannelFactory" />
+		/// </summary>
+		DataContractSerializer,
 
 		/// <summary>
 		/// XmlSerializer like but more close to legacy WCF WSDL structure
