2b18f30e1edb16e9fe090e93abb6faf56e0dd730
simonov <simonov@proitr.ru>
2020-01-29 10:38:59 +0500

    complete PipeReader only if its own pipe

--- src/SoapCore.Tests/ServiceOperationTuner/TestServiceOperationTuner.cs
+++ src/SoapCore.Tests/ServiceOperationTuner/TestServiceOperationTuner.cs
@@ -5,35 +5,42 @@ using SoapCore.Extensibility;
 
 namespace SoapCore.Tests.ServiceOperationTuner
 {
-	public class TestServiceOperationTuner : IServiceOperationTuner
-	{
-		public static bool IsCalled { get; private set; }
-		public static bool IsSetPingValue { get; private set; }
+    public class TestServiceOperationTuner : IServiceOperationTuner
+    {
+        public static bool IsCalled { get; private set; }
+        public static bool IsSetPingValue { get; private set; }
+        public static bool IsBodyAvailable { get; private set; }
 
-		public static void Reset()
-		{
-			IsCalled = false;
-			IsSetPingValue = false;
-		}
+        public static void Reset()
+        {
+            IsCalled = false;
+            IsSetPingValue = false;
+            IsBodyAvailable = false;
+        }
 
-		public void Tune(HttpContext httpContext, object serviceInstance, ServiceModel.OperationDescription operation)
-		{
-			IsCalled = true;
-			if ((serviceInstance != null) && (serviceInstance is TestService)
-				&& operation.Name.Equals("PingWithServiceOperationTuning"))
-			{
-				TestService service = serviceInstance as TestService;
-				string result = string.Empty;
+        public void Tune(HttpContext httpContext, object serviceInstance, ServiceModel.OperationDescription operation)
+        {
+            IsCalled = true;
+            if ((serviceInstance != null) && (serviceInstance is TestService)
+                && operation.Name.Equals("PingWithServiceOperationTuning"))
+            {
+                TestService service = serviceInstance as TestService;
+                string result = string.Empty;
 
-				StringValues pingValue;
-				if (httpContext.Request.Headers.TryGetValue("ping_value", out pingValue))
-				{
-					result = pingValue[0];
-				}
+                StringValues pingValue;
+                if (httpContext.Request.Headers.TryGetValue("ping_value", out pingValue))
+                {
+                    result = pingValue[0];
+                }
 
-				service.SetPingResult(result);
-				IsSetPingValue = true;
-			}
-		}
-	}
+                if (httpContext.Request.Body.CanRead)
+                {
+                    IsBodyAvailable = true;
+                }
+
+                service.SetPingResult(result);
+                IsSetPingValue = true;
+            }
+        }
+    }
 }
