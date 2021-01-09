de8d75287ba8b4e4ebf21d68db138250e48688c4
Jonas Jäderberg <jonas@jaderb.se>
2021-01-09 13:02:49 +0100

    First version with external WSDL

---
+++ src/SoapCore/WebServiceWSDLMapping.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoapCore
+{
+    public class WebServiceWSDLMapping
+    {
+        public string WsdlFile { get; set; }
+        public string WSDLFolder { get; set; }
+        public string SchemaFolder { get; set; }
+    }
+}
