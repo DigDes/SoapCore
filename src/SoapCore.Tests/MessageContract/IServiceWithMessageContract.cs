40b9539b42f299491bc2aea466c62eb9c25544c3
Jonas Jäderberg <jonas@jaderb.se>
2021-01-13 08:05:52 +0100

    Handle message contrac requests, wrapped correctly

--- src/SoapCore.Tests/MessageContract/IServiceWithMessageContract.cs
+++ src/SoapCore.Tests/MessageContract/IServiceWithMessageContract.cs
@@ -4,10 +4,19 @@ using SoapCore.Tests.OperationDescription.Model;
 
 namespace SoapCore.Tests.MessageContract
 {
-	[ServiceContract]
+	[ServiceContract(Namespace = "http://tempuri.org")]
 	public interface IServiceWithMessageContract
 	{
 		[OperationContract]
+		[XmlSerializerFormat(SupportFaults = true)]
 		string EmptyRequest(Model.MessageContractRequestEmpty req);
+
+		[OperationContract]
+		[XmlSerializerFormat(SupportFaults = true)]
+		Model.MessageContractResponse DoRequest(Model.MessageContractRequest req);
+
+		[OperationContract]
+		[XmlSerializerFormat(SupportFaults = true)]
+		Model.MessageContractResponseNotWrapped DoRequest2(Model.MessageContractRequestNotWrapped req);
 	}
 }
