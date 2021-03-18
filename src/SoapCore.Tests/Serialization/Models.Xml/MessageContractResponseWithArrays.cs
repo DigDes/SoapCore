c6ae68b56d00e3f5e82fcc870f8fd3b72476ae18
Ricky Kaare Engelharth <ren@delegate.dk>
2021-03-18 16:52:38 +0100

    Add failing test for MessageContracts with MessageHeader and Arrays

---
+++ src/SoapCore.Tests/Serialization/Models.Xml/MessageContractResponseWithArrays.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Xml.Serialization;
+
+namespace SoapCore.Tests.Serialization.Models.Xml
+{
+	[MessageContract(WrapperName = "MessageContractResponseWithArrays", WrapperNamespace = "http://xmlelement-namespace/", IsWrapped = true)]
+	public class MessageContractResponseWithArrays
+	{
+		[MessageBodyMember(Namespace = "http://xmlelement-namespace/", Order = 0)]
+		[XmlElement("ArrayWithoutContainers")]
+		public ComplexModel1[] ArrayWithoutContainers { get; set; }
+
+		[MessageBodyMember(Namespace = "http://xmlelement-namespace/", Order = 1)]
+		[XmlArray("ArrayWithContainers")]
+		[XmlArrayItem("ComplexModel1")]
+		public ComplexModel1[] ArrayWithContainers { get; set; }
+
+		[MessageBodyMember(Namespace = "http://xmlelement-namespace/", Order = 2)]
+		[XmlElement("ObjectArrayWithoutContainers")]
+		public ComplexObject[] ObjectArrayWithoutContainers { get; set; }
+
+		[MessageBodyMember(Namespace = "http://xmlelement-namespace/", Order = 3)]
+		[XmlArray("ObjectArrayWithContainers")]
+		[XmlArrayItem("ComplexObject")]
+		public ComplexObject[] ObjectArrayWithContainers { get; set; }
+
+		public static MessageContractResponseWithArrays CreateSample()
+			=> new MessageContractResponseWithArrays
+			{
+				ArrayWithoutContainers = new[]
+				{
+					ComplexModel1.CreateSample1(),
+					ComplexModel1.CreateSample2(),
+					ComplexModel1.CreateSample3()
+				},
+				ArrayWithContainers = new[]
+				{
+					ComplexModel1.CreateSample1(),
+					ComplexModel1.CreateSample2(),
+					ComplexModel1.CreateSample3()
+				},
+				ObjectArrayWithoutContainers = new[]
+				{
+					ComplexObject.CreateSample1(),
+					ComplexObject.CreateSample2(),
+					ComplexObject.CreateSample3(),
+				},
+				ObjectArrayWithContainers = new[]
+				{
+					ComplexObject.CreateSample1(),
+					ComplexObject.CreateSample2(),
+					ComplexObject.CreateSample3(),
+				},
+			};
+	}
+}
