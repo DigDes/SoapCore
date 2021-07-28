f3b56177f69d97a30c6a88e6af6fa337deca7053
Thomas Ibel <tibel@users.noreply.github.com>
2021-07-28 07:31:15 +0200

    remove usage of PipeStream and BufferTextWriter

--- src/SoapCore/MessageEncoder/Verify.cs
+++ src/SoapCore/MessageEncoder/Verify.cs
@@ -3,20 +3,9 @@ using System.Diagnostics;
 
 namespace SoapCore.MessageEncoder
 {
+	[Obsolete]
 	internal static class Verify
 	{
-		[DebuggerStepThrough]
-		public static void NotDisposed(PipeStream disposedValue)
-		{
-			Requires.NotNull(disposedValue, nameof(disposedValue));
-
-			if (disposedValue.IsDisposed)
-			{
-				string objectName = disposedValue.GetType().FullName;
-				throw new ObjectDisposedException(objectName);
-			}
-		}
-
 		[DebuggerStepThrough]
 		public static void Operation(bool condition, string message)
 		{
