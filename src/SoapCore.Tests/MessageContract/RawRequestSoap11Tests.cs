b0aec1e2d72b86a20ad0444710a331d8826ab364
Anders Jonsson <anjo@multinet.se>
2021-12-03 09:58:37 +0100

    cleanup of double test

--- src/SoapCore.Tests/MessageContract/RawRequestSoap11Tests.cs
+++ src/SoapCore.Tests/MessageContract/RawRequestSoap11Tests.cs
@@ -107,27 +107,6 @@ namespace SoapCore.Tests.MessageContract
 				//the result should be an empty array
 				Assert.IsTrue(resultMessage.Contains("<ArrayOfIntMethodResult />"));
 			}
-
-			const string body2 = @"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:tem=""http://tempuri.org"">
-  <soapenv:Header/>
-  <soapenv:Body>
-	<tem:ArrayOfIntMethod>
-    <tem:arrayOfIntParam><tem:int>1</tem:int><tem:int>2</tem:int></tem:arrayOfIntParam>
-	</tem:ArrayOfIntMethod>
-  </soapenv:Body>
-</soapenv:Envelope>
-";
-
-			using (var host = CreateTestHost(typeof(ArrayOfIntService)))
-			using (var client = host.CreateClient())
-			using (var content = new StringContent(body2, Encoding.UTF8, "text/xml"))
-			using (var res = await host.CreateRequest("/Service.asmx").AddHeader("SOAPAction", @"""ArrayOfIntMethod""").And(msg => msg.Content = content).PostAsync())
-			{
-				res.EnsureSuccessStatusCode();
-				var resultMessage = await res.Content.ReadAsStringAsync();
-				Assert.IsTrue(resultMessage.Contains("<int>1</int>"));
-				Assert.IsTrue(resultMessage.Contains("<int>2</int>"));
-			}
 		}
 
 		[TestMethod]
