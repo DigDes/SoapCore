5970927d51dca824458225dbd1af126db9c5b85d
Anders Jonsson <anjo@multinet.se>
2022-03-25 14:45:14 +0100

    Added test to show how to modify outbound message

--- src/SoapCore.Tests/SoapMessageProcessor/Startup.cs
+++ src/SoapCore.Tests/SoapMessageProcessor/Startup.cs
@@ -1,5 +1,8 @@
+using System.IO;
 using System.ServiceModel.Channels;
+using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -22,6 +25,18 @@ namespace SoapCore.Tests.SoapMessageProcessor
 				{
 					return Message.CreateMessage(MessageVersion.Soap11, "none");
 				}
+				else if (httpcontext.Request.Path.Value.Contains("ServiceWithPongProcessor.svc"))
+				{
+					var msg = await next(message);
+					var reader = msg.GetReaderAtBodyContents();
+
+					var content = await reader.ReadOuterXmlAsync();
+
+					var ms = new MemoryStream(Encoding.UTF8.GetBytes(content.Replace("Ping", "Pong")));
+					var xmlReader = XmlReader.Create(ms);
+
+					return Message.CreateMessage(msg.Version, null, xmlReader);
+				}
 				else
 				{
 					return await next(message);
@@ -37,6 +52,7 @@ namespace SoapCore.Tests.SoapMessageProcessor
 			app.UseEndpoints(x =>
 			{
 				x.UseSoapEndpoint<TestService>("/ServiceWithProcessor.svc", new SoapEncoderOptions(), SoapSerializer.DataContractSerializer);
+				x.UseSoapEndpoint<TestService>("/ServiceWithPongProcessor.svc", new SoapEncoderOptions(), SoapSerializer.DataContractSerializer);
 				x.UseSoapEndpoint<TestService>("/Service.svc", new SoapEncoderOptions(), SoapSerializer.DataContractSerializer);
 			});
 		}
