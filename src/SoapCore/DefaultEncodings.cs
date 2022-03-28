48dd9ecd9bd0b199715df46a51e25ca21c6ed1e3
Anders Jonsson <anjo@multinet.se>
2022-03-28 08:49:27 +0200

    removed BOM from the default Unicode encodings as well

--- src/SoapCore/DefaultEncodings.cs
+++ src/SoapCore/DefaultEncodings.cs
@@ -7,7 +7,7 @@ namespace SoapCore
 	internal class DefaultEncodings
 	{
 		public static Encoding UTF8 => new UTF8Encoding(false);
-		public static Encoding Unicode => Encoding.Unicode;
-		public static Encoding BigEndianUnicode => Encoding.BigEndianUnicode;
+		public static Encoding Unicode => new UnicodeEncoding(false, false);
+		public static Encoding BigEndianUnicode => new UnicodeEncoding(true, false);
 	}
 }
