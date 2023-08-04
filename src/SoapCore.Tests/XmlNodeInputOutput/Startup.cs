ba6bd9ae6ba8d74fbba49b67402b8432adc27db5
Raymond Mimick <rmimick@bfc-usa.com>
2023-08-04 16:40:21 -0500

    New Test Cases Written for XmlElement
    New Test Cases to check for XmlElement return type in both integration testing and as an XML SOAP message. Also, added an XmlElement input parameter to allow the code to accept a free-form Xml Document/Element

---
+++ src/SoapCore.Tests/XmlNodeInputOutput/Startup.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace SoapCore.Tests.XmlNodeInputOutput
+{
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddSoapCore();
+			services.TryAddSingleton<IXmlNodeInputOutput, XmlNodeInputOutput>();
+			services.AddMvc();
+		}
+
+		public void Configure(IApplicationBuilder app)
+		{
+			app.UseRouting();
+
+			app.UseEndpoints(x =>
+			{
+				x.UseSoapEndpoint<IXmlNodeInputOutput>("/Service.svc", new SoapEncoderOptions(), SoapSerializer.DataContractSerializer);
+				x.UseSoapEndpoint<IXmlNodeInputOutput>("/Service.asmx", new SoapEncoderOptions(), SoapSerializer.XmlSerializer);
+			});
+		}
+	}
+}
