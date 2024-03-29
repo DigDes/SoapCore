221e16e31d02824abe145b965e6825f7a80800ae
Dominik Baran <dominik.baran@guestline.com>
2024-03-29 16:15:45 +0100

    fix:(soap-endpoint): skip duplicated xml parameters

--- src/SoapCore/SoapEndpointMiddleware.cs
+++ src/SoapCore/SoapEndpointMiddleware.cs
@@ -676,6 +676,7 @@ namespace SoapCore
 		private object[] GetRequestArguments(Message requestMessage, XmlDictionaryReader xmlReader, OperationDescription operation, HttpContext httpContext)
 		{
 			var arguments = new object[operation.AllParameters.Length];
+			var alreadyProcessedParameters = new bool[operation.AllParameters.Length];
 
 			IEnumerable<Type> serviceKnownTypes = operation
 				.GetServiceKnownTypesHierarchy()
@@ -691,7 +692,7 @@ namespace SoapCore
 					while (!xmlReader.EOF)
 					{
 						var parameterInfo = operation.InParameters.FirstOrDefault(p => p.Name == xmlReader.LocalName);
-						if (parameterInfo == null)
+						if (parameterInfo == null || alreadyProcessedParameters[parameterInfo.Index])
 						{
 							xmlReader.Skip();
 							continue;
@@ -704,6 +705,7 @@ namespace SoapCore
 						}
 
 						lastParameterIndex = parameterInfo.Index;
+						alreadyProcessedParameters[lastParameterIndex] = true;
 
 						var argumentValue = _serializerHelper.DeserializeInputParameter(
 							xmlReader,
