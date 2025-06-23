46d15e054c584e9bcbb9b181e81202ab17bbc422
Serban Apostol <serban.apostol@rbc.com.ro>
2025-06-23 11:14:02 +0300

    Configured AuthorizeOperationMessageProcessor in SoapCore.Tests to use CaseInsensitivePath

--- src/SoapCore.Tests/Startup.cs
+++ src/SoapCore.Tests/Startup.cs
@@ -45,16 +45,17 @@ namespace SoapCore.Tests
 					};
 				});
 
-			services.AddSoapMessageProcessor(new AuthorizeOperationMessageProcessor(new Dictionary<string, Type>
-			{
-				{ "/Service.svc", typeof(TestService) },
-				{ "/ServiceCI.svc", typeof(TestService) },
-				{ "/Service.asmx", typeof(TestService) },
-				{ "/WSA10Service.svc", typeof(TestService) },
-				{ "/WSA11ISO88591Service.svc", typeof(TestService) },
-				{ "/ServiceWithDifferentEncodings.asmx", typeof(TestService) },
-				{ "/ServiceWithOverwrittenContentType.asmx", typeof(TestService) },
-			}));
+			services.AddSoapMessageProcessor(new AuthorizeOperationMessageProcessor(
+				new Dictionary<string, Type>
+				{
+					{ "/Service.svc", typeof(TestService) },
+					{ "/ServiceCI.svc", typeof(TestService) },
+					{ "/Service.asmx", typeof(TestService) },
+					{ "/WSA10Service.svc", typeof(TestService) },
+					{ "/WSA11ISO88591Service.svc", typeof(TestService) },
+					{ "/ServiceWithDifferentEncodings.asmx", typeof(TestService) },
+					{ "/ServiceWithOverwrittenContentType.asmx", typeof(TestService) },
+				}, options => options.CaseInsensitivePath = true));
 			services.AddAuthorization(options =>
 			{
 				options.AddPolicy("something", policy => policy.RequireClaim("someclaim", "somevalue"));
