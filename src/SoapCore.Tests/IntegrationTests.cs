7c5c5191ff20b0b609a1e1237c1df05f478a4712
lice <lice@multinet.se>
2026-06-09 13:31:42 +0200

    removed some changes that are not required

--- src/SoapCore.Tests/IntegrationTests.cs
+++ src/SoapCore.Tests/IntegrationTests.cs
@@ -453,11 +453,11 @@ namespace SoapCore.Tests
 			throw new InvalidOperationException("Unreachable code");
 		}
 
-		private ITestService CreateClient(bool caseInsensitivePath = false, string port = "5050")
+		private ITestService CreateClient(bool caseInsensitivePath = false)
 		{
 			var binding = new BasicHttpBinding();
 			var endpoint = new EndpointAddress(new Uri(
-				$"http://localhost:{port}/{(caseInsensitivePath ? "serviceci" : "Service")}.svc"));
+				string.Format("http://{0}:5050/{1}.svc", "localhost", caseInsensitivePath ? "serviceci" : "Service")));
 			var channelFactory = new ChannelFactory<ITestService>(binding, endpoint);
 			var serviceClient = channelFactory.CreateChannel();
 			return serviceClient;
